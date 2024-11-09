b9187996b35be785f254593493f59af85cfc81df
Roshan-Poudel <rowshanpoudel@gmail.com>
2024-11-10 00:04:58 +0100

    ipv6 compatability and fixes in the code

--- python-packet-sniffer.py
+++ python-packet-sniffer.py
@@ -3,35 +3,54 @@
 from scapy.all import *
 import socket
 import datetime
-import os
-import time
 
-def network_monitoring_for_visualization_version(pkt):
-	time=datetime.datetime.now()
-		#classifying packets into TCP
-	if pkt.haslayer(TCP):
-		# classyfying packets into TCP Incoming packets
-		if socket.gethostbyname(socket.gethostname())== pkt[IP].dst:
-			print(str("[")+str(time)+str("]")+"  "+"TCP-IN:{}".format(len(pkt[TCP]))+" Bytes"+"    "+"SRC-MAC:" +str(pkt.src)+"    "+ "DST-MAC:"+str(pkt.dst)+"    "+ "SRC-PORT:"+str(pkt.sport)+"    "+"DST-PORT:"+str(pkt.dport)+"    "+"SRC-IP:"+str(pkt[IP].src  )+"    "+"DST-IP:"+str(pkt[IP].dst  ))
-	
-		if socket.gethostbyname(socket.gethostname())==pkt[IP].src:
-			print(str("[")+str(time)+str("]")+"  "+"TCP-OUT:{}".format(len(pkt[TCP]))+" Bytes"+"    "+"SRC-MAC:" +str(pkt.src)+"    "+ "DST-MAC:"+str(pkt.dst)+"    "+ "SRC-PORT:"+str(pkt.sport)+"    "+"DST-PORT:"+str(pkt.dport)+"    "+"SRC-IP:"+str(pkt[IP].src)+"    "+"DST-IP:"+str(pkt[IP].dst))
-	#classifying packets into UDP	
-	if pkt.haslayer(UDP):
-		if socket.gethostbyname(socket.gethostname())==pkt[IP].src:
-			# classyfying packets into UDP Outgoing packets
-			print(str("[")+str(time)+str("]")+"  "+"UDP-OUT:{}".format(len(pkt[UDP]))+" Bytes "+"    "+"SRC-MAC:" +str(pkt.src)+"    "+"DST-MAC:"+ str(pkt.dst)+"    "+"SRC-PORT:"+ str(pkt.sport) +"    "+"DST-PORT:"+ str(pkt.dport)+"    "+"SRC-IP:"+ str(pkt[IP].src)+"    "+"DST-IP:"+ str(pkt[IP].dst))
-	   
-		if socket.gethostbyname(socket.gethostname())==pkt[IP].dst:
-			# classyfying packets into UDP Incoming packets
-			print(str("[")+str(time)+str("]")+"  "+"UDP-IN:{}".format(len(pkt[UDP]))+" Bytes "+"    "+"SRC-MAC:" +str(pkt.src)+"    "+"DST-MAC:"+ str(pkt.dst)+"    "+"SRC-PORT:"+ str(pkt.sport) +"    "+"DST-PORT:"+ str(pkt.dport)+"    "+"SRC-IP:"+ str(pkt[IP].src)+"    "+"DST-IP:"+ str(pkt[IP].dst))
-		#classifying packets into ICMP
-	if pkt.haslayer(ICMP):
-		# classyfying packets into UDP Incoming packets
-		if socket.gethostbyname(socket.gethostname())==pkt[IP].src:
-			print(str("[")+str(time)+str("]")+"  "+"ICMP-OUT:{}".format(len(pkt[ICMP]))+" Bytes"+"    "+"IP-Version:"+str(pkt[IP].version) +"    "*1+" SRC-MAC:"+str(pkt.src)+"    "+"DST-MAC:"+str(pkt.dst)+"    "+"SRC-IP: "+str(pkt[IP].src)+ "    "+"DST-IP:  "+str(pkt[IP].dst))	
-							 
-		if socket.gethostbyname(socket.gethostname())==pkt[IP].dst:
-			print(str("[")+str(time)+str("]")+"  "+"ICMP-IN:{}".format(len(pkt[ICMP]))+" Bytes"+"    "+"IP-Version:"+str(pkt[IP].version)+"    "*1+"	 SRC-MAC:"+str(pkt.src)+"    "+"DST-MAC:"+str(pkt.dst)+"    "+"SRC-IP: "+str(pkt[IP].src)+ "    "+"DST-IP:  "+str(pkt[IP].dst))	
+def get_local_ip():
+    # Get the local network IP address of the host
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+
+# Local IP address of the machine
+local_ip = get_local_ip()
+
+def network_monitoring(pkt):
+    # Capture the current timestamp
+    timestamp = datetime.datetime.now()
+
+    # Check for TCP packets with IP or IPv6 layers
+    if pkt.haslayer(TCP):
+        if pkt.haslayer(IP):
+            ip_layer = IP
+        elif pkt.haslayer(IPv6):
+            ip_layer = IPv6
+        else:
+            return  
+
+        # Determine if it's an incoming or outgoing TCP packet
+        direction = "IN" if pkt[ip_layer].dst == local_ip else "OUT"
+        print(f"[{timestamp}] TCP-{direction}: {len(pkt[TCP])} Bytes "
+              f"SRC-MAC: {pkt.src} DST-MAC: {pkt.dst} "
+              f"SRC-PORT: {pkt.sport} DST-PORT: {pkt.dport} "
+              f"SRC-IP: {pkt[ip_layer].src} DST-IP: {pkt[ip_layer].dst}")
+
+    # Check for UDP packets with IP layer
+    elif pkt.haslayer(UDP) and pkt.haslayer(IP):
+        # Determine if it's an incoming or outgoing UDP packet
+        direction = "IN" if pkt[IP].dst == local_ip else "OUT"
+        print(f"[{timestamp}] UDP-{direction}: {len(pkt[UDP])} Bytes "
+              f"SRC-MAC: {pkt.src} DST-MAC: {pkt.dst} "
+              f"SRC-PORT: {pkt.sport} DST-PORT: {pkt.dport} "
+              f"SRC-IP: {pkt[IP].src} DST-IP: {pkt[IP].dst}")
+
+    # Check for ICMP packets with IP layer
+    elif pkt.haslayer(ICMP) and pkt.haslayer(IP):
+        # Determine if it's an incoming or outgoing ICMP packet
+        direction = "IN" if pkt[IP].dst == local_ip else "OUT"
+        print(f"[{timestamp}] ICMP-{direction}: {len(pkt[ICMP])} Bytes "
+              f"IP-Version: {pkt[IP].version} "
+              f"SRC-MAC: {pkt.src} DST-MAC: {pkt.dst} "
+              f"SRC-IP: {pkt[IP].src} DST-IP: {pkt[IP].dst}")
+
 if __name__ == '__main__':
-	sniff(prn=network_monitoring_for_visualization_version)
+    print(f"Starting network monitoring on local IP: {local_ip}")
+    sniff(prn=network_monitoring)
